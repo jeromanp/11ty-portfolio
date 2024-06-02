8b98b52d4fa3451c0796f2190fc0ac756fa80097
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-02 15:15:57 -0600

    restart .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,17 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
-const { data } = require("autoprefixer");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "global.out.css": "global.css",
   });
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-  eleventyConfig.addPassthroughCopy("src/img");
-
   return {
     passthroughFileCopy: true,
     dir: {
       input: "src",
-      includes: "_includes",    
-      data:"_data", 
+      includes: "_includes",
+      data: "_data",
       output: "_site",
-      
     },
   };
 };
